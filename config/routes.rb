fe4872d2f613ff33307c01e3a50404762c131976
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2016-02-29 15:11:57 -0500

    Format the created blog display

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  # root to: 'visitors#index'
+  root to: 'articles#index'
+  get 'visitors', to: 'visitors#index'
   
   devise_for :users
 
