fac85b5981934d6f923c63601f9dfb73c0b94483
John Paul Bamberg <jpbamberg1993@gmail.com>
2016-04-08 18:00:26 -0400

    Describe page used as reference for layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   mount RedactorRails::Engine => '/redactor_rails'
   root to: 'articles#index'
+
+  # Leave until decided on side widget well
   get 'visitors', to: 'visitors#index'
-  
+
   devise_for :users
 
   resources :users
