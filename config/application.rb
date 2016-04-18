03a10acb344471f104b7619101dd679bf5b27cf6
John Paul Bamberg <jpbamberg1993@gmail.com>
2016-04-17 22:19:12 -0400

    Serve static assets

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,6 @@ module Strat7
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_files = true
   end
 end
