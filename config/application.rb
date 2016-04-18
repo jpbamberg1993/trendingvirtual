0de287a6b27c28f9ea7775162027c1e3147e263a
John Paul Bamberg <jpbamberg1993@gmail.com>
2016-04-18 15:49:31 -0400

    Remove serve static assets

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,5 @@ module Strat7
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_files = true
   end
 end
