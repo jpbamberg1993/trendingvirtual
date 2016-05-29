8ea3b1055ec31f3cbe9adcec9efea87b9b02fe9c
John Paul Bamberg <jpbamberg1993@gmail.com>
2016-05-29 11:06:37 -0400

    Enable comments to use gravatar and username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420032945) do
+ActiveRecord::Schema.define(version: 20160529145455) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,11 +22,13 @@ ActiveRecord::Schema.define(version: 20160420032945) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "commenter_id"
+    t.string   "email"
+    t.string   "username"
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
