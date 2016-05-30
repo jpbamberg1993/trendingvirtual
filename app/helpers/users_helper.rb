cd1588e0164de2605ef374c1e8bc5741da2fe78f
John Paul Bamberg <jpbamberg1993@gmail.com>
2016-05-29 20:13:24 -0400

    Style comment section

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,6 @@ module UsersHelper
   def gravatar_for(email)
     gravatar_id = Digest::MD5::hexdigest(email)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: email, class: "gravatar")
+    image_tag(gravatar_url, alt: email, class: "gravatar img-responsive")
   end
 end
