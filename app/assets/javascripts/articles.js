859d048d7c7d777b36edd2d82565e96483c25a3a
John Paul Bamberg <jpbamberg1993@gmail.com>
2017-06-11 15:16:11 -0400

    Don’t show carousel on paginated pages

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,9 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(
+    function() {
+        const pc = Number(document.querySelector('li.active a').innerText);
+        if(pc > 1) $('#bs-carousel').hide();
+    }
+);
