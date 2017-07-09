bf93aded57312a2201997133d01567dc31d7db2c
John Paul Bamberg <jpbamberg1993@gmail.com>
2017-07-09 11:37:26 -0400

    Hide carousel on pages after one

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,8 +2,8 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).ready(
-    function() {
-        const pc = Number(document.querySelector('li.active a').innerText);
-        if(pc > 1) $('#bs-carousel').hide();
-    }
+  function() {
+    const pc = Number(document.querySelector('li.active a').innerText);
+    if(pc > 1) $('#bs-carousel').hide();
+  }
 );
